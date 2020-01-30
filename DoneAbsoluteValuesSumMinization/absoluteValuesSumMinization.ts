2622ab6b835c67b5836b48920ed45f997b9c2bf2
Nazar <matvienkont@outlook.com>
2020-01-30 14:08:45 +0200

    absoluteValuesSumMinization

---
+++ DoneAbsoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -0,0 +1,10 @@
+function absoluteValuesSumMinimization(a: number[]): boolean {
+    const isEven: boolean = a.length/2 == true;
+
+    return isEven;  
+}
+
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
